75f7caf6ab8eff2964b33c79df393713c2941157
Saint-Jean Germila <germila@noukod.com>
2019-01-18 15:27:12 +0000

    Automatically backed up by Learn

--- app/application_controller.rb
+++ app/application_controller.rb
@@ -1,9 +1,9 @@
  require 'sinatra/base'
   require './app'
-  
+
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, Proc.new { File.join(root, "./views/") }
 
 get '/' do
  erb :super_hero

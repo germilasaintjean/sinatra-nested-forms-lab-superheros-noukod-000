7dadaff64f2b39b5be995e2490369bfbec255f0e
Saint-Jean Germila <germila@noukod.com>
2019-01-18 15:28:50 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -3,8 +3,8 @@ class Hero
   attr_accessor :name, :power, :biography
 
   def initialize(name, power, biography)
-    @name=name
-    @power=power
+    @name= name
+    @power= power
     @biography= biography
   end
 end
